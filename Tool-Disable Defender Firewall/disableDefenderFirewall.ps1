4beb34d337ab4c1f12084e8d9d80f7cb9d6f4615
Richard Smith <p4729a@gmail.com>
2025-01-03 16:07:42 -0800

    Initial Sync

---
+++ Tool-Disable Defender Firewall/disableDefenderFirewall.ps1
@@ -0,0 +1,7 @@
+﻿# Disable Defender Firewall on WinSvr2022
+
+# Disable Windows Defender Firewall service on Windows Server 2022 for all network profiles (domain, private, and public).
+netsh advfirewall set allprofiles state off
+
+# Verify Windows Defender Firewall is off for all networks
+netsh advfirewall show all
